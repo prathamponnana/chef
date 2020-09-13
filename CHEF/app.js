810316fea615d7b48a6191d1c6fd05375a15f975
PRATHAM PONNANA <46704977+prathamponnana@users.noreply.github.com>
2020-09-13 18:15:02 +0530

    Update app.js

--- CHEF/app.js
+++ CHEF/app.js
@@ -1,14 +1,13 @@
-const ui = new UI();
-
 document.querySelector("form").addEventListener("submit", function (e) {
-  const inpItem = document.querySelector(".item").value;
+  const inpItem = document.querySelector(".inpItem").value;
   
   const food = new Food(inpItem);
+  const ui = new UI();
 
   food.getData()
-  .then(data => {
-    if(data === null) ui.showErr('Please try a different ingredient...');
-    else ui.showRes(data);
+  .then(recepieList => {
+    if(recepieList === null) ui.showErr('Please try a different ingredient...');
+    else ui.showRes(recepieList);
   })
   .catch(err => ui.showErr('Please check your internet connection...'));
 
